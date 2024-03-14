b3843a4398598c799bc533d4f803088d34b9784f
Alexis <alexiscontamin@gmail.com>
2024-03-14 08:07:11 +0100

    ajout theme

---
+++ App/lib/core/theme/colors.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+
+class AppColors {
+
+  static const Color white = Color(0xFFFFFFFF);
+
+
+  static const Color black = Color(0xFF000000);
+
+
+  static const Color primary = Colors.green;
+
+
+  static Color primary75 = primary.withOpacity(0.75);
+
+
+  static Color primary50 = primary.withOpacity(0.50);
+
+
+  static Color primary25 = primary.withOpacity(0.25);
+
+
+  static const Color secondary = Color(0xFF00BFA5);
+
+
+  static const Color backgroundColor = Color(0xF5F5F5F5);
+
+
+  static const Color grey100 = Color(0xFFE0E0E0);
+
+
+  static const Color grey300 = Color(0xFFD4D4D4);
+
+
+  static const Color grey400 = Color(0xFF737373);
+
+
+  static const Color grey500 = Color(0xFF737373);
+
+
+  static const Color grey600 = Color(0xFF525252);
+
+
+  static const Color error = Color(0xFFD32F2F);
+
+}
+
