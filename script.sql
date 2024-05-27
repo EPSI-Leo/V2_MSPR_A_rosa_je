b2beb6455e1bed723970ec8076ab3602c4783a76
Alexis <106440915+moi-sixela@users.noreply.github.com>
2024-05-27 09:56:46 +0200

    new change for sql script

--- script.sql
+++ script.sql
@@ -1,56 +1,25 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1:3306
--- Generation Time: Mar 03, 2024 at 08:22 AM
--- Server version: 5.7.36
--- PHP Version: 7.4.26
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `arosaje`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `advices`
---
-
 USE arosaje;
 
 DROP TABLE IF EXISTS `advices`;
-CREATE TABLE IF NOT EXISTS `advices` (
+DROP TABLE IF EXISTS `plants`;
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE IF NOT EXISTS `users` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `id_user` int,
-  `id_plant` int,
-  `name` varchar(100) DEFAULT NULL,
-  `advice` text,
-  PRIMARY KEY (`id`),
-  CONSTRAINT `fk_advices_users` FOREIGN KEY (`id_user`) REFERENCES `users`(`id`),
-  CONSTRAINT `fk_advices_plants` FOREIGN KEY (`id_plant`) REFERENCES `plants`(`id`)
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `username` varchar(80) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `cgu` tinyint(1) NOT NULL,
+  `role` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `advices`
---
--- --------------------------------------------------------
-
---
--- Table structure for table `plants`
---
-
-DROP TABLE IF EXISTS `plants`;
 CREATE TABLE IF NOT EXISTS `plants` (
   `id` int NOT NULL AUTO_INCREMENT,
   `id_user` int NOT NULL,
@@ -62,34 +31,18 @@ CREATE TABLE IF NOT EXISTS `plants` (
   `latitude` double NOT NULL,
   `longitude` double NOT NULL,
   PRIMARY KEY (`id`),
-  CONSTRAINT `fk_plants_users` FOREIGN KEY (`id_user`) REFERENCES `users`(`id`)
+  FOREIGN KEY (`id_user`) REFERENCES `users`(`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `plants`
---
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (
+CREATE TABLE IF NOT EXISTS `advices` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `first_name` varchar(50) NOT NULL,
-  `last_name` varchar(50) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `username` varchar(80) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `cgu` int NOT NULL,
-  `role` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`)
+  `id_user` int,
+  `id_plant` int,
+  `name` varchar(100) DEFAULT NULL,
+  `advice` text,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`id_user`) REFERENCES `users`(`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  FOREIGN KEY (`id_plant`) REFERENCES `plants`(`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
---
--- Dumping data for table `users`
---
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+COMMIT;
